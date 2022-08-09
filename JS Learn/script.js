ca1801970546f9690a1979f90fb9a4977abc38b4
Shanil <Shanilanees@gmail.com>
2022-08-09 07:11:02 +0500

    strings start

--- JS Learn/script.js
+++ JS Learn/script.js
@@ -1,8 +1,18 @@
 // document.getElementById("count").innerText = 5;
 
-
 let count = 0;
+console.log(count)
 
-function increment(){
-    console.log("The button is clicked")
+function increment() {
+  count += 1;
+  console.log((document.getElementById("count").innerText = count));
+}
+
+let save = 0;
+
+function save_function() {
+  save = count;
+  console.log((count ,document.getElementById("count").innerText = save));
+  count = 0;
+
 }
