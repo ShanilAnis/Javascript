df19f427f313ddc0a0cd154b24ebe36c49c1d51c
Shanil <Shanilanees@gmail.com>
2022-09-10 21:32:47 +0500

    add event listener

--- Chrome Extension/script.js
+++ Chrome Extension/script.js
@@ -1,3 +1,12 @@
-function saveInput(){
-    console.log("Button Clicked")
-}
+const inputBtn = document.getElementById("input-btn")
+let myLeads = ['googlen','japan','jjfjj']
+const inputEl = document.getElementById("input-el")
+
+
+inputBtn.addEventListener('click', function(){
+    myLeads.push(inputEl.value) 
+    for(let i = 0; i < myLeads.length; i++){
+        console.log(myLeads[i])
+    }
+})
+
